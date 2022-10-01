c08516b0f5cf8f321afda88b1950cf338aa02656
Robin7D4 <robinv175@gmail.com>
2022-10-01 12:59:06 +0200

    Main menu
    Implemented main menu and settings menu basic layout and and functionality to quit, settings and back buttons

---
+++ Assets/MenuController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    //Method creating a new game
+    public void NewGame()
+    {
+
+    }
+    //Methon continuing a game from a save file
+    public void ContinueGame()
+    {
+
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
