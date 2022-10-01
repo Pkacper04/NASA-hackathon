77e0e2da6abbe34812d228d86bfa476c08855ce1
Vojaker <arkadiuszwojas@gmail.com>
2022-10-01 16:12:42 +0200

    ciekawostki skrypt

---
+++ Assets/Ciekawostki.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ciekawostk : MonoBehaviour
+{ 
+
+}
+
+public enum curisities
+{
+    WebbObserve, //Webb is the ideal telescope to study brown dwarfs and better understand their nature. A brown dwarf is a strange cosmic object that can be considered a "failed star": not massive enough to be a star, but more massive than a planet.
+
+    HowWebbWorks, //Webb uses infrared light, which cannot be perceived by the human eye, to study every phase in cosmic history. The telescope's four scientific instruments are specifically designed to capture infrared light, and are able to peer through cosmic dust to study colder or very distant objects.
+
+    WhereIsWebb,  //In addition to orbiting the Sun, Webb makes a tight orbit around a point in space known as Lagrange 2, or L2. This point is located 1.5 million kilometres from Earth. Its distant location gives Webb an unobstructed view of the sky, and aids its ability to see much farther into the universe than the Hubble Space Telescope.
+
+    FirstLaunch, //Launching in 1990, Hubble Space Telescope was the largest space telescope in history. However, James Webb Space Telescope (JWST) took over this record in 2018. At the size of a typical school bus, Hubble will be dwarfed by JWST’s tennis court size.
+
+    GoldenMirror,//Gold layers plating the scope’s mirrors are only 1,000 atoms thick. Indeed, only a golf-ball-sized amount of gold was used to coat the entire 21-foot-wide mirror.
+
+    WebbTemperature,    //First, the side of JWST that will always face the Sun will constantly sit at a scorching 185º F. However, the other side, containing the mirrors and instruments, will constantly sit at a frigid -388º F. 
+
+}
