db318b40662ea1a44b12e4b45a6c048f7060181b
Luuq <lukasz2zagaja@gmail.com>
2022-10-02 08:50:33 +0200

    ChangingSceneWorking

--- Assets/Scripts/LevelLoader/LevelLoader.cs
+++ Assets/Scripts/LevelLoader/LevelLoader.cs
@@ -1,18 +1,24 @@
 using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class LevelLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadLevel(int sceneIndex)
     {
-        
+        StartCoroutine(LoadAsynchronously(sceneIndex));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadAsynchronously (int sceneIndex)
     {
-        
+        AsyncOperation operation= SceneManager.LoadSceneAsync(sceneIndex);
+
+        while(!operation.isDone)
+        {
+            Debug.Log(operation.progress);
+
+            yield return null;
+        }
     }
 }
